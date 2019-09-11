b1355cac36e801c53330c9e22ce379ecb062bb42
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-11 22:11:27 +0100

    added implementation for view All Teams

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -3,13 +3,13 @@ const User = require('../models/user.model');
 const sendResponse = require('../helpers/response');
 const TeamModel = require('../models/team.model');
 
-
-exports.viewAllTeams = async(req,res,next) => {
-  
+exports.viewAllTeams = async (req, res, next) => {
   try {
-res.send('view all teams testing')
-    
+    const teams = await TeamModel.find();
+    res.json(
+      sendResponse(httpStatus.OK, 'These are a list of all the teams', teams)
+    );
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
