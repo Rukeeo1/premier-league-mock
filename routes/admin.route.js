35c6d765a6926a7a2bc7fe9521cc3bc9e9c6436f
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-11 18:39:15 +0100

    added route to verify admin

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,16 +4,19 @@ const adminCtrl = require('../controllers/admin.controller');
 const userValidation = require('../validations/user.validation');
 const teamValidation = require('../validations/team.validation');
 const verifyToken = require('../helpers/verifyToken')
+const  verifyAdmin = require('../middlewares/verifyAdmin.middleware')
+
 
 
 
 
 const router = express.Router();
 
-
 router.route('/').post(validate(userValidation.signUp, { abortEarly: false }), adminCtrl.createAdmin)
 
 router.use(verifyToken)
+
+router.use(verifyAdmin)
 //create team route...
 /** api/v1/admin/add-team */
 router.route('/add-team').post(validate(teamValidation.addTeam, { abortEarly: false }), adminCtrl.addTeam)
