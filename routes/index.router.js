d3a856a4c0689b1debf8229d30b540f421afd385
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-11 07:37:48 +0100

    mounted user routes on index.js...and signup working

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const userRoutes = require('./user.route')
+
 
 
 
 //check the api...
 router.get('/health-check', (req, res) => res.send('OK'));
+
+
+//mount user route...api/v1/user
+router.use('/user', userRoutes)
 
 module.exports = router;
