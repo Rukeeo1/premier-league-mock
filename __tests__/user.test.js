9fd30e00ed2fc9bba1f4e38fa1d47fdff560d134
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-11 12:20:36 +0100

    added test to user.test.js

---
+++ __tests__/user.test.js
@@ -0,0 +1,64 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../server');
+const User = require('../models/user.model');
+
+let userFour;
+
+beforeEach(async () => {
+  await User.deleteMany().exec();
+  let userOne = {
+    name: 'Rukee Ojigbo',
+    email: 'rukeeojigbo@gmail.com',
+    password: '123456',
+    favoriteTeam: 'Manchester United'
+  };
+
+  let userTwo = {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    password: '123456',
+    favoriteTeam: 'Chealsea Fc'
+  };
+
+  let userThree = {
+    name: 'Nkem Alozie',
+    email: 'nkem@gmail.com',
+    password: '123456',
+    favoriteTeam: 'Arsenal FC'
+  };
+
+  userFour = {
+    name: 'Jane Doe',
+    email: 'janedoe@gmail.com',
+    password: '123456',
+    favoriteTeam: 'Rashford FC'
+  };
+  afterAll(async () => {
+    mongoose.connection.close();
+    app.close();
+  });
+
+  userOne = await new User(userOne).save();
+  userTwo = await new User(userTwo).save();
+  userThree = await new User(userThree).save();
+});
+
+afterAll(async () => {
+  await User.deleteMany();
+  mongoose.connection.close();
+});
+
+describe('#USER: test for user route', () => {
+  describe('Health check', () => {
+    it('should return OK for health-check', async () => {
+      await request(app)
+        .get('/api/v1/health-check/')
+        .then(res => {
+          console.log(res.body);
+          expect(res.text).toMatch(/OK/);
+          expect(res.status).toBe(200);
+        });
+    });
+  });
+});
