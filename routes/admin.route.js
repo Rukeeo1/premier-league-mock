1fb7c5d8e108b6ad59389e9dd8dce77d5c5d9b82
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-11 13:28:39 +0100

    added route to add team for admin

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,11 +2,18 @@ const express = require('express');
 const { celebrate: validate, errors } = require('celebrate');
 const adminCtrl = require('../controllers/admin.controller');
 const userValidation = require('../validations/user.validation');
+const teamValidation = require('../validations/team.validation');
+
 
 
 
 const router = express.Router();
 
 router.route('/').post(validate(userValidation.signUp, { abortEarly: false }), adminCtrl.createAdmin)
+
+//create team route...
+/** api/v1/admin/add-team */
+router.route('/add-team').post(validate(teamValidation.addTeam, { abortEarly: false }), adminCtrl.addTeam)
+
 
 module.exports =  router;
