4b390c87b1708d3beec07531dd390a6c04a2c577
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-11 15:01:55 +0100

    added verify token to protect route

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,14 +3,17 @@ const { celebrate: validate, errors } = require('celebrate');
 const adminCtrl = require('../controllers/admin.controller');
 const userValidation = require('../validations/user.validation');
 const teamValidation = require('../validations/team.validation');
+const verifyToken = require('../helpers/verifyToken')
 
 
 
 
 const router = express.Router();
 
+
 router.route('/').post(validate(userValidation.signUp, { abortEarly: false }), adminCtrl.createAdmin)
 
+router.use(verifyToken)
 //create team route...
 /** api/v1/admin/add-team */
 router.route('/add-team').post(validate(teamValidation.addTeam, { abortEarly: false }), adminCtrl.addTeam)
