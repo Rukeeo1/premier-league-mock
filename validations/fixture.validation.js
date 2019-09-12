dafce0487da511642fcc38cd7f330cbb07c2f230
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-12 05:45:07 +0100

    added validation for shema creation

---
+++ validations/fixture.validation.js
@@ -0,0 +1,20 @@
+const { Joi } = require('celebrate');
+
+module.exports = {
+  //add Fixture
+  createFixture: {
+    body: {
+      date: Joi.string().required(),
+      time: Joi.string().required(),
+      status: Joi.string(),
+      refree: Joi.string(),
+      venue: Joi.string().required(),
+      homeTeam: Joi.string().required(),
+      awayTeam: Joi.string().required(),
+      goalsHomeTeam: Joi.string(),
+      goalsAwayTeam: Joi.string(),
+      fullTimeScores: Joi.string(),
+      result: Joi.string()
+    }
+  }
+};
