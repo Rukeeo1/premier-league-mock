40d8ed9e8d0337d6dc0df8904918bfdf49356a58
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-12 07:52:34 +0100

    added controller for get fixtures

--- controllers/fixture.controller.js
+++ controllers/fixture.controller.js
@@ -1,28 +1,40 @@
 const httpStatus = require('http-status');
 const User = require('../models/user.model');
 const sendResponse = require('../helpers/response');
-const Fixture = require('../models/fixture.model')
+const Fixture = require('../models/fixture.model');
 
 exports.addFixture = async (req, res, next) => {
   try {
     const { homeTeam, awayTeam, venue } = req.body;
-console.log(homeTeam);
+
 
     //if hometeam, awayteam, and venue already exist...break..
     const fixtureExist = await Fixture.find({ homeTeam, awayTeam, venue });
 
     if (fixtureExist.length) {
-      return res.json(sendResponse(httpStatus.BAD_REQUEST, 'this fixture already exists'))
+      return res.json(
+        sendResponse(httpStatus.BAD_REQUEST, 'this fixture already exists')
+      );
     }
-
 
     const fixture = new Fixture(req.body);
 
     await fixture.save();
 
-    return res.json(sendResponse(httpStatus.OK, "fixture successfully create", fixture))
-
+    return res.json(
+      sendResponse(httpStatus.OK, 'fixture successfully create', fixture)
+    );
   } catch (error) {
-    next(error)
+    next(error);
   }
+};
+
+exports.removeFixture = async (req, res, next) => {
+res.json('testing remove fixtures')
 }
+
+
+exports.getFixtures = async (req, res, next) => {
+ res.json('testing get fixtures ')
+}
+
