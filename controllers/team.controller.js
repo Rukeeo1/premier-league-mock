0944621b9064a3c0ef5c96410e844f6b3ad38a71
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-13 12:17:20 +0100

    implemented redis on view all teams

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -2,16 +2,34 @@ const httpStatus = require('http-status');
 const User = require('../models/user.model');
 const sendResponse = require('../helpers/response');
 const TeamModel = require('../models/team.model');
-const client  = require('../redis/redis')
-
-
+const client = require('../redis/redis');
 
 exports.viewAllTeams = async (req, res, next) => {
   try {
-    const teams = await TeamModel.find();
-    res.json(
-      sendResponse(httpStatus.OK, 'These are a list of all the teams', teams)
-    );
+    const redisTeamsKeys = 'teamRedis';//redis key..
+    return client.get(redisTeamsKeys, async (err, teams) => {
+      //if teams exists in redis cache...return teams
+      if (teams) {
+        return res.json(
+          sendResponse(
+            httpStatus.OK,
+            'These are a list of all from redisthe teams',
+            JSON.parse(teams)
+          )
+        );
+      } else {//else find teams from the database and first set redis key...
+        const teams = await TeamModel.find();
+        client.setex(redisTeamsKeys,360,JSON.stringify(teams))
+        //return normal response to client...
+        res.json(
+          sendResponse(
+            httpStatus.OK,
+            'These are a list of all the teams',
+            teams
+          )
+        );
+      }
+    });
   } catch (error) {
     next(error);
   }
@@ -20,41 +38,39 @@ exports.viewAllTeams = async (req, res, next) => {
 exports.getTeam = async (req, res, next) => {
   try {
     const team = await TeamModel.findById(req.params.id);
-    
-    if(!team) {
-      return res.json(sendResponse(httpStatus.BAD_REQUEST,'The team not found' ))
+
+    if (!team) {
+      return res.json(
+        sendResponse(httpStatus.BAD_REQUEST, 'The team not found')
+      );
     }
 
-  return   res.json(sendResponse(httpStatus.OK,team))
-  
+    return res.json(sendResponse(httpStatus.OK, team));
   } catch (error) {
-   next(error) 
+    next(error);
   }
-}
+};
 
-exports.search = async (req,res,next) => {
+exports.search = async (req, res, next) => {
   try {
-
     const team = await TeamModel.search(req.query.search);
 
-    if(!team){
-      return res.json(sendResponse(httpStatus.BAD_REQUEST, 'Team not found'))
+    if (!team) {
+      return res.json(sendResponse(httpStatus.BAD_REQUEST, 'Team not found'));
     }
 
-    res.json(sendResponse(httpStatus.OK,'This is the Team',team))
+    res.json(sendResponse(httpStatus.OK, 'This is the Team', team));
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.removeTeam = async (req, res, next) => {
-  
-    try {
-      const { id } = req.params;
-      let team = await TeamModel.findByIdAndRemove(id);
-      res.json(sendResponse(httpStatus.OK, 'Team sucessfully removed', team))
-    } catch (error) {
-      next(error)
-    
+  try {
+    const { id } = req.params;
+    let team = await TeamModel.findByIdAndRemove(id);
+    res.json(sendResponse(httpStatus.OK, 'Team sucessfully removed', team));
+  } catch (error) {
+    next(error);
   }
-}
+};
