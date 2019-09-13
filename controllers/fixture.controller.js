e66c5aeba9769daf4e9816087251c5eb9ca88a57
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-13 07:40:03 +0100

    rearranged controllers... get single fixture bug fixed

--- controllers/fixture.controller.js
+++ controllers/fixture.controller.js
@@ -20,92 +20,73 @@ exports.addFixture = async (req, res, next) => {
 
     await fixture.save();
 
-  
     return res.json(
       sendResponse(httpStatus.OK, 'fixture successfully created', fixture)
     );
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
     next(error);
   }
 };
 
 exports.removeFixture = async (req, res, next) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
 
     const fixture = await Fixture.findByIdAndRemove(id);
 
-    return res.json(sendResponse(httpStatus.OK, 'Successfully deleted', fixture));
-
-  } catch (error) {
-    next(error)
-  }
-};
-
-exports.getFixtures = async (req, res, next) => {
-  try {
-    const fixtures = await Fixture.find();
-    res.json(
-      sendResponse(httpStatus.OK, 'These are all fixtures', fixtures)
+    return res.json(
+      sendResponse(httpStatus.OK, 'Successfully deleted', fixture)
     );
   } catch (error) {
     next(error);
   }
 };
 
-exports.getSingleFixture = async (req, res, next) => {
+exports.getFixtures = async (req, res, next) => {
   try {
-    const { id } = req.params
-
-    const fixture = await Fixture.get(id);
-
-    if (!fixture) {
-      return res.json(sendResponse(httpStatus.BAD_REQUEST, 'Fixture not found'), fixture)
-    }
-
-    res.json(sendResponse(httpStatus.OK, fixture))
-  } catch (error) {
-    next(error)
-  }
-}
-
-exports.getPendingFixtures = async (req, res, next) => {
-  try {
-    const pendingFixtures = await Fixture.find({ status: "Pending" });
-
-    res.json(sendResponse(httpStatus.OK, pendingFixtures))
-  } catch (error) {
-    next(error)
-  }
-}
-
-exports.getCompletedFixtures = async (req, res, next) => {
-  try {
-    const pendingFixtures = await Fixture.find({ status: "Completed" });
-
-    res.json(sendResponse(httpStatus.OK, pendingFixtures))
-  } catch (error) {
-    next(error)
-  }
-}
-
-exports.search = async (req, res, next) => {
-  try {
-    console.log(req.params,'from controller')
-    const fixture = await Fixture.search(req.query.search)
-console.log(fixture,'hleeee')
-    if (!fixture) {
-      return res.json(sendResponse(httpStatus.OK, 'Fixture not found'));
-    }
-console.log(fixture);
-
-    res.json(sendResponse(httpStatus.OK, fixture))
+    const fixtures = await Fixture.find();
+    res.json(sendResponse(httpStatus.OK, 'These are all fixtures', fixtures));
   } catch (error) {
     next(error);
   }
 };
 
+exports.getPendingFixtures = async (req, res, next) => {
+  try {
+    const pendingFixtures = await Fixture.find({ status: 'Pending' });
+
+    res.json(sendResponse(httpStatus.OK, pendingFixtures));
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getCompletedFixtures = async (req, res, next) => {
+  try {
+    const pendingFixtures = await Fixture.find({ status: 'Completed' });
+
+    res.json(sendResponse(httpStatus.OK, pendingFixtures));
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.search = async (req, res, next) => {
+  try {
+    console.log(req.params, 'from controller');
+    const fixture = await Fixture.search(req.query.search);
+    console.log(fixture, 'hleeee');
+    if (!fixture) {
+      return res.json(sendResponse(httpStatus.OK, 'Fixture not found'));
+    }
+    
+
+    res.json(sendResponse(httpStatus.OK, fixture));
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.updateFixture = async (req, res, next) => {
   try {
@@ -114,17 +95,34 @@ exports.updateFixture = async (req, res, next) => {
     let fixture = await Fixture.findById(id);
 
     if (!fixture) {
-      return res.sendResponse(httpStatus[400], "Fixture doesn't exists")
+      return res.sendResponse(httpStatus[400], "Fixture doesn't exists");
     }
 
-    fixture = await fixture.update(req.body)
+    fixture = await fixture.update(req.body);
 
     fixture.save();
 
-    return res.json(sendResponse(httpStatus.OK, 'Team details updated successfully', fixture));
-
+    return res.json(
+      sendResponse(httpStatus.OK, 'Team details updated successfully', fixture)
+    );
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
+exports.getSingleFixture = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const fixture = await Fixture.get(id);
+
+    // if (!fixture) {
+    //   return res.json(sendResponse(httpStatus.BAD_REQUEST, 'Fixture not found'), fixture)
+    // }
+    if (fixture) {
+      res.json(sendResponse(httpStatus.OK, fixture));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
