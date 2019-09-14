5dcd64eb808e3e7d805fbbaf66dba5574d0d10b0
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-14 19:59:26 +0100

    refactor redis client

--- redis/redis.js
+++ redis/redis.js
@@ -1,12 +1,9 @@
 const redis = require('redis'); //required redis
 
 const client = redis.createClient({
-  host: process.env.REDIS_CLIENT_HOST,
-  port: 13384,
-  no_ready_check: true,
-  auth_pass: process.env.REDIS_LAB_AUTH_PASS
+  host: 'localhost',
+  port: 6379,
 }); //created redis client
-
 // echo redis errors to the console
 client.on('error', err => {
   //logged redis error to the console...
