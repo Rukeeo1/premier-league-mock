9b897d4521b8201edb2d11af64e1689b7597ae37
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-09-14 09:24:41 +0100

    refactored redis client

--- redis/redis.js
+++ redis/redis.js
@@ -1,10 +1,17 @@
-const redis = require('redis');//required redis
+const redis = require('redis'); //required redis
 
+const client = redis.createClient({
+  host: process.env.REDIS_CLIENT_HOST,
+  port: 13384,
+  no_ready_check: true,
+  auth_pass: process.env.REDIS_LAB_AUTH_PASS
+}); //created redis client
 
-const client = redis.createClient(6379,'localhost');//created redis client
 // echo redis errors to the console
-client.on('error', (err) => {//logged redis error to the console...
-  console.log("Error " + err)
+client.on('error', err => {
+  //logged redis error to the console...
+  console.log('Error ' + err);
 });
 
 module.exports = client;
+//6397 LOCAL HOST
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const redis = require('redis');//required redis
+// const redis = require('redis');//required redis
 
 const dotenv = require('dotenv');
 dotenv.config();
